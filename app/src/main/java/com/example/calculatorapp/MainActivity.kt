441d0ff18903c118e6f729e18b8def9955162d48
Beratcan Güreş <guresberatcan@gmail.com>
2021-05-23 07:30:16 +0300

    Started OnDigit

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -3,16 +3,21 @@ package com.example.calculatorapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val tvInput = findViewById<View>(R.id.tvInput)
     }
 
     fun onDigit(view: View){
-        Toast.makeText(this,"Button Works",Toast.LENGTH_SHORT).show()
+        val tvInput = findViewById<TextView>(R.id.tvInput)
+        tvInput.append("1")
+
     }
 
 }
