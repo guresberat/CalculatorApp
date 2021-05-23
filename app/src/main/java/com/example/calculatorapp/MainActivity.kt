4118a37e5c6d6dc1f8e1288009747d906331e3d1
Beratcan Güreş <guresberatcan@gmail.com>
2021-05-23 08:15:45 +0300

    Blocked multiple operator usage

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -9,6 +9,9 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
+    var lastNumeric : Boolean = false
+    var lastDot : Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,11 +20,39 @@ class MainActivity : AppCompatActivity() {
     fun onDigit(view: View){
         val tvInput = findViewById<TextView>(R.id.tvInput)
         tvInput.append((view as Button).text)
+        lastNumeric = true
     }
 
     fun onClear(view: View){
         val tvInput = findViewById<TextView>(R.id.tvInput)
         tvInput.text =" "
+        lastNumeric = false
+        lastDot = false
+    }
+
+    fun onDecimalPoint(view: View){
+        val tvInput = findViewById<TextView>(R.id.tvInput)
+        if(lastNumeric && !lastDot){
+            tvInput.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
+    }
+
+    fun onOperator(view : View){
+        val tvInput = findViewById<TextView>(R.id.tvInput)
+        if(lastNumeric && !isOperatorAdded(tvInput.text.toString()))
+            tvInput.append((view as Button).text)
+            lastNumeric = false
+            lastDot = false
+    }
+
+    private fun isOperatorAdded(value: String) : Boolean{
+            return if(value.startsWith("-")){
+                false
+            }else{
+                value.contains("/") || value.contains("*") || value.contains("+") || value.contains("-")
+            }
     }
 
 }
